57dce20295994d454c6bca3046d87b560c3dfcc7
Ian Magonya <ianbenard2002@gmail.com>
2023-11-24 15:41:14 +0300

    initial commit

---
+++ scr/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/scr/main.css">
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="d-flex justify-content-between">
+        <div><p class="h1">TechImisha</p></div>
+        <div>
+            <nav class="navbar">
+                <div class="container-fluid">
+                    <ul>
+                        <li class="nav-item">
+                            <a href="#">Programs</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li class="nav-item">
+                            <a href="#">Admissions</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li class="nav-item">
+                            <a href="#">About</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li class="nav-item">
+                            <a href="#">Media</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+  </div>  
+</body>
+</html>
