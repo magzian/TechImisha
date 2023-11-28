0e1fd9b642e7f56e328c5e99769fc795408dd1cf
Ian Magonya <ianbenard2002@gmail.com>
2023-11-28 07:21:48 +0300

    techImisha

---
+++ scr/components/hero.php
@@ -0,0 +1,9 @@
+<div class="carousel-inner" role="list-box">
+      <div class="carousel-item active" style="height: 554px;">
+      <video src="../scr/vid/mixkit-young-students-copying-the-blackboard-in-a-classroom-28315-medium.mp4" class="object-fit-contain w-100 " autoplay loop muted></video>
+    <div class="border border-secondary d-flex-column carousel-caption font-weight-bold text-warning p-5">
+      <h2 class="justify-content-start">Tech Training For Digital Future</h2>
+      <button type="button" class="btn btn-warning btn-lg mt-3 text-white">Learn More</button>
+    </div>
+    </div>
+  </div>
--- scr/index.php
+++ scr/index.php
@@ -1,42 +1,35 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TechImisha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/scr/main.css">
 </head>
-<body>
-  <div class="container-fluid">
-    <div class="d-flex justify-content-between">
-        <div><p class="h1">TechImisha</p></div>
-        <div>
-            <nav class="navbar">
-                <div class="container-fluid">
-                    <ul>
-                        <li class="nav-item">
-                            <a href="#">Programs</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav-item">
-                            <a href="#">Admissions</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav-item">
-                            <a href="#">About</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav-item">
-                            <a href="#">Media</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div>
-  </div>  
-</body>
+
+ <?php
+    include('components/header.php')
+?>
+
+<?php
+  include('components/hero.php')
+?>
+
+
+
+
+
+
+
+  
+</div>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="/assets/js/jquery-3.7.1.min.js"></script>
+  </body>
 </html>
+
+
+    
+    
+ 
