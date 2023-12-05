115290e137100fdcc29d63c3073aebd8784092ea
Ian Magonya <ianbenard2002@gmail.com>
2023-12-05 19:10:24 +0300

    TechImisha

--- scr/components/hero.php
+++ scr/components/hero.php
@@ -1,9 +1,19 @@
 <div class="carousel-inner" role="list-box">
       <div class="carousel-item active" style="height: 554px;">
       <video src="../scr/vid/mixkit-young-students-copying-the-blackboard-in-a-classroom-28315-medium.mp4" class="object-fit-contain w-100 " autoplay loop muted></video>
-    <div class="border border-secondary d-flex-column carousel-caption font-weight-bold text-warning p-5">
+    <div class="d-flex-column carousel-caption font-weight-bold text-warning p-5">
       <h2 class="justify-content-start">Tech Training For Digital Future</h2>
-      <button type="button" class="btn btn-warning btn-lg mt-3 text-white">Learn More</button>
+  <form action="../scr/dbcon.php" method="POST">   
+  <div class="form-floating mb-3">
+  <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input name="pass" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>
+<button type="submit" class="btn btn-outline-warning mt-5">SUBMIT</button>
+</form> 
     </div>
     </div>
   </div>
--- scr/dbcon.php
+++ scr/dbcon.php
@@ -5,9 +5,23 @@
  $password = '';
  $database = 'dbdetails';
 
- $con = mysqli_connect($host,$username,$password,$database);
+ $conn = mysqli_connect($host,$username,$password,$database);
 
- if(!$con){
+ if(!$conn){
     die("Connection Failed ". mysqli_connect_error());
  }
+
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+$sql = "INSERT INTO studdetails(email,pass) VALUES ('$email','$pass')";
+
+if(mysqli_query($conn,$sql)){
+   echo "Record submitted";
+} else{
+   echo "Error".$mysli_error($conn);
+}
+
+mysqli_close($conn);
+
 ?>
